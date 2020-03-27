52d1dd0170fb9aaa3d9d57a533b6d9b371092662
julian <jepotes@gmail.com>
2020-03-26 23:12:30 -0300

    Final Modificado

---
+++ commons/cabecera2.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Fresca|Roboto&display=swap" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="./css/mdb.min.css" rel="stylesheet">
+  <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!--Main Navigation-->
+  <header>
+  
+  <!--Navbar -->
+<nav class=" navbar navbar-expand-lg navbar-dark white fixed-top">
+  <a class="navbar-brand" href="#navbarSupportedContent-333"><img src="img/Logo_Final_MTP.png" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="barras"><i class="fas fa-bars fa-1x"></i></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+    <ul class="navbar-nav mr-auto">
+    <hr class="linea"> 
+      <li class="nav-item active  text-center">
+        <a class="nav-link" href="#navbarSupportedContent-333" >Inicio
+          <span class="sr-only">actual</span>
+        </a>
+      </li>
+      <hr class="linea"> 
+      <li class="nav-item  text-center">
+        <a class="nav-link" href="#f-cot" >Cotizar</a>
+      </li>
+      <hr class="linea">
+      <li class="nav-item ">
+        <a class="nav-link  text-center" href="#" >Preguntas</a>
+      </li>
+      <hr class="linea"> 
+    </ul>
+    <ul class="navbar-nav ml-auto nav-flex-icons float-right">
+      <li class="nav-item">
+        <a class="nav-link waves-effect waves-light">
+          <i class="fab fa-facebook fa-2x"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link waves-effect waves-light">
+          <i class="fab fa-google-plus-g  fa-2x"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--/.Navbar -->
+
+    <!--Intro Section-->
+    <section class="view intro-2 blue-gradient">
+      <div class="mask">
+        <div class="container h-100 mb-3">
+          <div class="row flex-center pt-5 mt-3 p-2">
+            <div class="col-md-12 col-lg-6 text-center text-md-left margins pt-3">
+              <div class="white-text">
+                <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Pagá menos por el seguro de tu Moto.</h1>
+                <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+                <h6 class="wow fadeInLeft" data-wow-delay="0.3s">Hasta 30% menos en el seguro de tu Moto. Las mismas condiciones y con las mejores compañías.
+                </h6>
+                <br>
+                <a class="btn btn-outline-white btn-rounded font-weight-bold ml-lg-0 wow fadeInLeft"
+                  data-wow-delay="0.3s" href="#formPreview">BUSCA EL MEJOR PRECIO</a>
+               
+              </div>
+            </div>
+
+            <div class="col-md-12 col-lg-6 d-flex wow fadeInRight" data-wow-delay="0.3s">
+           
+            <div class="fondo align-items-center pb-3">
+            <div class="personaje mt-2 align-items-center"></div>      
+            </div>
+            </div>
+            
+           
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <span class="ir-arriba z-depth-4"><i class="fas fa-angle-up mdb-gallery-view-icon"></i></span>
+  </header>
+  <!--Main Navigation-->
+
+ 
