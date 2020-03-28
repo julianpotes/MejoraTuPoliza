a6741c987cf943d75857dbac90dfd9b074257d32
julian <jepotes@gmail.com>
2020-03-28 00:05:41 -0300

    PRODUCTRES DESPLEGABLE

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include("commons/cabecera.php");
 ?>
   <!--Main content-->
-  <div class="container-fluid">
+  <div class="container-fluid mt-4">
   <main>
-  <img class="wave" src="img/onda.png">
+  
 	<div class="containerjuli bg-white">
 		<div class="imagenj">
 			<img src="img/undraw_by_my_car_ttge.svg">
@@ -12,7 +12,7 @@ include("commons/cabecera.php");
 		<div class="login-content">
 			<form action="index.html">
 				<img src="img/undraw_profile_pic_ic5t.svg">
-				<h2 class="title">Mejaramos Yu Poliza</h2>
+				<h2 class="title">Mejoramos Tu Poliza</h2>
            		<div class="input-div one"> 
            		   <div class="jul">
            		   		<i class="fas fa-user"></i>
@@ -41,19 +41,89 @@ include("commons/cabecera.php");
 				 	</div>
 				  </div>
 
-				  <div class="input-div selector">
-					<div class="jul"> 
-						 <i class="fa fa-users"></i>
-					</div>
-					<select style="">
-						  <option>Productor 1</option>
-						  <option>Productor 2</option>
-						  <option>Productor 3</option>
-						  <option>Productor 4</option>
-						</select>
-			  	</div>
-			  
-            	<a href="#">Envianos Whatsap?</a>
+			
+				  <div class="container-fluid">
+					  <div class="row">
+					  <div class="contenedorselect">
+	
+								<div class="selectbox">
+									<div class="select" id="select">
+										<div class="contenido-select">
+											<h1 class="titulo">Lista De productores</h1>
+											<p class="descripcion">Elegi segun tu comodidad.</p>
+										</div>
+										<i class="fas fa-angle-down"></i>
+									</div>
+
+									<div class="opciones" id="opciones">
+										<a href="#" class="opcion">
+											<div class="contenido-opcion">
+												<div class="textos">
+													<img src="img/prod1.png" alt="">
+													<h1 class="titulo">Productor-1</h1>
+													<p class="descripcion">direccion.</p>
+												</div>
+											</div>
+										</a>
+										<a href="#" class="opcion">
+											<div class="contenido-opcion">
+												<img src="img/prod2.png" alt="">
+												<div class="textos">
+												<h1 class="titulo">Productor-2</h1>
+													<p class="descripcion">direccion.</p>
+												</div>
+											</div>
+										</a>
+										<a href="#" class="opcion">
+											<div class="contenido-opcion">
+												<img src="img/prod3.png" alt="">
+												<div class="textos">
+												<h1 class="titulo">Productor-3</h1>
+													<p class="descripcion">direccion.</p>
+												</div>
+											</div>
+										</a>
+										<a href="#" class="opcion">
+											<div class="contenido-opcion">
+												<img src="img/prod4.png" alt="">
+												<div class="textos">
+												<h1 class="titulo">Productor-4</h1>
+													<p class="descripcion">direccion.</p>
+												</div>
+											</div>
+										</a>
+										<a href="#" class="opcion">
+											<div class="contenido-opcion">
+												<img src="img/prod5.png" alt="">
+												<div class="textos">
+												<h1 class="titulo">Productor-5</h1>
+													<p class="descripcion">direccion.</p>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+
+								<input type="hidden" name="pais" id="inputSelect" value="">
+
+
+							</div>
+					  </div>
+				  </div>
+				  
+        
+          <div data-template="mFileInputTemp" data-popover="file" class="pb-5" style="position: relative; left: 0px; top: 0px;" data-original-title="" title="">
+                          <div class="file-field">
+                            <div class="btn btn-primary btn-sm float-left ">
+                                <span style="font-size:0.8rem;">Elija Documento</span>
+                                  <input type="file" id="fileInput">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" style="font-size:.8rem;" type="text" placeholder="Subir">
+                            </div>
+                          </div>              
+                        </div>  
+            	<a href="#">Queres Enviarnos Un Whatsapp?</a>
             	<input type="submit" class="btnjuli" value="Enviar">
             </form>
         </div>
--- js/main.js
+++ js/main.js
@@ -18,3 +18,25 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+//SELECTOR PRSONALIZADO
+
+const select = document.querySelector('#select');
+const opciones = document.querySelector('#opciones');
+const contenidoSelect = document.querySelector('#select .contenido-select');
+const hiddenInput = document.querySelector('#inputSelect');
+
+document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenidoSelect.innerHTML = e.currentTarget.innerHTML;
+		select.classList.toggle('active');
+		opciones.classList.toggle('active');
+		hiddenInput.value = e.currentTarget.querySelector('.titulo').innerText;
+	});
+});
+
+select.addEventListener('click', () => {
+	select.classList.toggle('active');
+	opciones.classList.toggle('active');
+});
